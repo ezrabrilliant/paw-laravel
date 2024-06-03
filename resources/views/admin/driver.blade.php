62c03ff30652468101bbb9b806ef3e3a1e0f4b91
Ezra Brilliant <64943463+ezrabrilliant@users.noreply.github.com>
2024-06-03 23:19:51 +0700

    Update driver.blade.php

--- resources/views/admin/driver.blade.php
+++ resources/views/admin/driver.blade.php
@@ -1,79 +1,79 @@
 @extends('app')
 
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
-    <script defer src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-    <script defer src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+<script defer src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script defer src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
 
 @section('title', 'Driver')
 
 @section('content')
+<div class="container" style="padding: 40px 0 20px 0;">
+    <div class="card animate template-paw text-white mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold"><strong>Riwayat Pembelian User</strong></h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" style="background-color: rgba(0, 0, 0, 0) !important" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Invoice ID</th>
+                            <th>Alamat</th>
+                            <th>Harga Delivery</th>
+                            <th>Tanggal</th>
+                            <th>Jenis Layanan</th>
+                            <th>Status Invoice</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($filteredInvoices as $index => $invoice)
+                        <tr>
+                            <td>{{ $invoice->invoice_id }}</td>
+                            <td>{{ $invoice->alamat }}</td>
+                            <td>{{ $invoice->harga_delivery }}</td>
+                            <td>{{ $invoice->tanggal }}</td>
+                            <td>{{ $invoice->layanan }}</td>
+                            <td>{{ $invoice->statusText }}</td>
+                            <td>
+                                {{-- log ke console, isi dari button --}}
+                                @if ($invoice->button == "Disabled")
 
-            <div class="container" style="padding: 40px 0 20px 0;">
-                <div class="card animate template-paw text-white mb-4">
-                    <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold"><strong>Riwayat Pembelian User</strong></h6>
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                        <table class="table table-bordered" id="dataTable" width="100%" style="background-color: rgba(0, 0, 0, 0) !important" cellspacing="0">
-                                <thead>
-                                    <tr>
-                                        <th>Invoice ID</th>
-                                        <th>Alamat</th>
-                                        <th>Harga Delivery</th>
-                                        <th>Tanggal Masuk</th>
-                                        <th>Tanggal Keluar</th>
-                                        <th>Jenis Layanan</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($filteredInvoices as $index => $invoice)
-                                    <tr>
-                                        <td>{{ $invoice->invoice_id  }}</td>
-                                        <td>{{ $invoice->alamat }}</td>
-                                        <td>{{ $invoice->harga_delivery }}</td>
-                                        <td>{{ $invoice->tanggal_masuk }}</td>
-                                        <td>{{ $invoice->tanggal_keluar }}</td>
-                                        <td>{{ $invoice->layanan }}</td>
-                                        <td>
-                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                                {{ $invoice->layanan }} Pesanan ini
-                                            </button>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+                                <button type="button" class="btn btn-secondary" disabled>{{ $invoice->button }}</button>
+                                @else
+                                    <button type="button" class="btn btn-primary confirmButton" data-invoice-id="{{ $invoice->invoice_id }}" data-newStatus="{{ $invoice->newStatus }}">{{ $invoice->button }}</button>
+                                @endif
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
+        </div>
+    </div>
 </div>
 
 <script>
-
     $(document).ready(function () {
-            $('#dataTable').DataTable();
+        $('#dataTable').DataTable();
     });
 
-
     $(document).ready(function() {
-
-        function fetchUpdatePesanan(invoice_id, layanan) {
+        function fetchUpdatePesanan(invoiceId, newStatus) {
             $.ajax({
                 url: '{{ route('updatePesanan') }}',
                 method: 'POST',
                 data: {
                     _token: '{{ csrf_token() }}',
                     invoice_id: invoiceId,
-                    layanan: layanan
+                    newStatus: newStatus
                 },
                 success: function(data) {
                     if (data.error) {
                         showAlert(data.error);
                     } else {
-                        showAlert('Gagal mengupdate pesanan.');
+                        showAlert('Pesanan berhasil diupdate');
+                        location.reload();
                     }
                 },
                 error: function(xhr, status, error) {
@@ -82,6 +82,15 @@
             });
         }
 
+        function showAlert(message) {
+            alert(message);
+        }
+
+        $(document).on('click', '.confirmButton', function() {
+            const invoiceId = $(this).data('invoice-id');
+            const newStatus = $(this).data('newstatus');
+            fetchUpdatePesanan(invoiceId, newStatus);
+        });
     });
 </script>
 @endsection
